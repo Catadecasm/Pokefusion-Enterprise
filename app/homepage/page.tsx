84160531d77dc351fd0d6051f3b4a5b3c2954608
catadecasm <Juliana-Catalina.De@endava.com>
2023-09-27 17:24:20 -0500

    feature/PKE15-homepage-main-menu done

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 export default function Homepage(){
     return <h1>Hello Homepage!</h1>
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import './global.css';
 export default function RootLayout({children}) {
   return <html>
       <head>
