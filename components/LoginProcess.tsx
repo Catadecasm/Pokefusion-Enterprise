a074f619b761612dea8967e6348d3d3e9f88bf16
catadecasm <Juliana-Catalina.De@endava.com>
2023-09-28 13:23:34 -0500

    feature/PKE8-login-process-correct-credentials created

--- components/LoginProcess.tsx
+++ components/LoginProcess.tsx
@@ -25,11 +25,11 @@ export default function SignIn() {
           </h1>
 
           <h1 className="text-[31px] font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Sign in to
+          Log in to
           </h1>
 
           <h4 style={{ fontSize: '16px' }} className="font-light leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Please enter your credentials to Sign in.
+          Please enter your credentials to log in.
           </h4>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
@@ -43,17 +43,7 @@ export default function SignIn() {
                   required
                 />
               </div>
-              <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Enter your email"
-                  required
-                />
-              </div>
+              
               <div>
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Password
@@ -97,10 +87,10 @@ export default function SignIn() {
                 type="submit"
                 className="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
               >
-                Sign in
+                Login
               </button>
               <p className="text-sm font-light text-center text-gray-500 dark:text-gray-400">
-                Don’t have an account yet? <a href="#" className="font-semibold text-black hover:underline dark:text-primary-500">Register</a>
+                Don’t have an account yet? <a href="/subscribe" className="font-semibold text-black hover:underline dark:text-primary-500">Register</a>
               </p>
             </form>
           </div>
