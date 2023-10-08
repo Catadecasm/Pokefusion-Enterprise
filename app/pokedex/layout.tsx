16c70c0e9e21407ff4407a6ca54febf90865d96f
catadecasm <Juliana-Catalina.De@endava.com>
2023-10-08 17:44:27 -0500

    feature/PKE20-pokedex-page-search-criteria done

--- app/pokedex/layout.tsx
+++ app/pokedex/layout.tsx
@@ -18,7 +18,7 @@ export default function Pokedex({children}) {
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
       </Head>
       <SideBarMenu/>
-      <SearchBar />
+      
         <div>
           <Pokemon />
         </div>
--- components/Pokemon.tsx
+++ components/Pokemon.tsx
@@ -1,25 +1,40 @@
 "use client";
-import {BiArrowBack} from "react-icons/bi";
-import {GrLinkNext} from "react-icons/gr";
+
+import SearchBar from "./SearchBar";
+import { BiArrowBack } from "react-icons/bi";
+import { GrLinkNext } from "react-icons/gr";
 import React, { useEffect, useState } from "react";
-import axios from "axios"; // Importa axios para realizar la solicitud HTTP
+import axios from "axios";
 
 export default function Pokemon() {
-  const [pokemonList, setPokemonList] = useState([]); // Estado para almacenar la lista de Pokémon
-  const [currentPage, setCurrentPage] = useState(0); // Estado para el número de página actual
+  const [pokemonList, setPokemonList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [filter, setFilter] = useState("");
+  const [totalPokemonCount, setTotalPokemonCount] = useState(0);
 
   useEffect(() => {
-    // Función para cargar la lista de Pokémon desde la API
+    const fetchTotalPokemonCount = async () => {
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        setTotalPokemonCount(response.data.count);
+      } catch (error) {
+        console.error("Error al obtener la cantidad total de Pokémon:", error);
+      }
+    };
+
+    fetchTotalPokemonCount();
+  }, []);
+
+  useEffect(() => {
     const fetchPokemonList = async () => {
       try {
+        const offset = currentPage * 24;
+        const limit = Math.min(24, totalPokemonCount - offset); // Asegurarse de no exceder la cantidad total
         const response = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon?offset=${
-            currentPage * 24 
-          }&limit=24`
+          `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
         );
         const results = response.data.results;
 
-        // Mapea los resultados para obtener los detalles de cada Pokémon
         const pokemonDetails = await Promise.all(
           results.map(async (pokemon) => {
             const detailResponse = await axios.get(pokemon.url);
@@ -27,90 +42,86 @@ export default function Pokemon() {
           })
         );
 
-        // Actualiza el estado con la lista de Pokémon
         setPokemonList(pokemonDetails);
       } catch (error) {
         console.error("Error al obtener la lista de Pokémon:", error);
       }
     };
 
-    // Llamada inicial para cargar la primera página de resultados
     fetchPokemonList();
-  }, [currentPage]);
+  }, [currentPage, totalPokemonCount]);
 
-  // Función para cargar la página siguiente
   const loadNextPage = () => {
     setCurrentPage(currentPage + 1);
   };
 
-  // Función para cargar la página anterior
   const loadPreviousPage = () => {
     if (currentPage > 0) {
       setCurrentPage(currentPage - 1);
     }
   };
 
-
+  const handleSearch = (value) => {
+    setFilter(value);
+  };
 
   return (
     <>
-      
-
-      {/* Lista de Pokémon */}
-    <div className="flex items-center justify-center pt-6">
-  <div className="flex flex-wrap justify-center w-[690px] h-[562px] px-3 pt-6 bg-white rounded-lg shadow-inner justify-start items-start gap-2 inline-flex">
-    {pokemonList.map((pokemon) => (
-      <div
-        key={pokemon.id}
-        className="w-[104px] h-[108px] bg-white rounded-lg shadow flex-col justify-between items-center inline-flex relative"
-      >
-        <div className="self-stretch px-2 pt-1 justify-end items-start gap-2 inline-flex">
-          <div className="text-right text-stone-500 text-[8px] font-normal font-Poppins leading-3">
-            #{pokemon.id}
-          </div>
-        </div>
-        <div className="w-[72px] h-[72px] justify-center items-center inline-flex mb-5">
-          <img
-            className="w-[72px] h-[72px] z-10"
-            src={pokemon.sprites.front_default}
-            alt={`Pokemon #${pokemon.id}`}
-            style={{ imageRendering: 'smooth' }}
-          />
-        </div>
-        <div className="w-full bg-zinc-100 absolute bottom-0 left-0">
-          <div className="w-22 h-11 px-2 pt-6 pb-1 rounded-md justify-start items-start">
-            <div className="grow shrink basis-0 text-center text-stone-900 text-xs font-normal font-Poppins leading-none">
-              {pokemon.name}
-            </div>
-          </div>
+      <SearchBar handleSearch={handleSearch} filter={filter} />
+      <div className="flex items-center justify-center pt-6">
+        <div className="flex flex-wrap justify-center w-[690px] h-[562px] px-3 pt-6 bg-white rounded-lg shadow-inner justify-start items-start gap-2 inline-flex">
+          {pokemonList
+            .filter(
+              (pokemon) =>
+                pokemon.name.toLowerCase().includes(filter.toLowerCase())
+            )
+            .map((pokemon) => (
+              <div
+                key={pokemon.id}
+                className="w-[104px] h-[108px] bg-white rounded-lg shadow flex-col justify-between items-center inline-flex relative"
+              >
+                <div className="self-stretch px-2 pt-1 justify-end items-start gap-2 inline-flex">
+                  <div className="text-right text-stone-500 text-[8px] font-normal font-Poppins leading-3">
+                    #{pokemon.id}
+                  </div>
+                </div>
+                <div className="w-[72px] h-[72px] justify-center items-center inline-flex mb-5">
+                  <img
+                    className="w-[72px] h-[72px] z-10"
+                    src={pokemon.sprites.front_default}
+                    alt={`Pokemon #${pokemon.id}`}
+                    style={{ imageRendering: "smooth" }}
+                  />
+                </div>
+                <div className="w-full bg-zinc-100 absolute bottom-0 left-0">
+                  <div className="w-22 h-11 px-2 pt-6 pb-1 rounded-md justify-start items-start">
+                    <div className="grow shrink basis-0 text-center text-stone-900 text-xs font-normal font-Poppins leading-none">
+                      {pokemon.name}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
-    ))}
-  </div>
-</div>
 
-
-
-
-<nav>
+      <nav>
         <ul className="flex justify-center">
           <li>
             <a
-                  className="mx-1 flex h-9 w-9 items-center justify-center rounded-full border border-blue-gray-100 bg-white p-0 text-sm text-blue-gray-500 transition duration-150 ease-in-out hover:bg-light-300"
-
+              className="mx-1 flex h-9 w-9 items-center justify-center rounded-full border border-blue-gray-100 bg-white p-0 text-sm text-blue-gray-500 transition duration-150 ease-in-out hover:bg-light-300"
               href="#"
               aria-label="Previous"
               onClick={loadPreviousPage}
             >
-              <span className="material-icons text-sm"><BiArrowBack/></span>
+              <span className="material-icons text-sm">
+                <BiArrowBack />
+              </span>
             </a>
           </li>
-         
+
           <li>
-            <a
-              className="mx-1 flex h-9 w-9 items-center justify-center rounded-full border border-blue-500 bg-blue-500 p-0 text-sm text-blue-gray-500 transition duration-150 ease-in-out hover:bg-light-300"
-              
-            >
+            <a className="mx-1 flex h-9 w-9 items-center justify-center rounded-full border border-blue-500 bg-blue-500 p-0 text-sm text-blue-gray-500 transition duration-150 ease-in-out hover:bg-light-300">
               {currentPage + 1}
             </a>
           </li>
@@ -121,15 +132,13 @@ export default function Pokemon() {
               aria-label="Next"
               onClick={loadNextPage}
             >
-              <span className="material-icons text-sm"><GrLinkNext/></span>
+              <span className="material-icons text-sm">
+                <GrLinkNext />
+              </span>
             </a>
           </li>
         </ul>
       </nav>
-
-      
     </>
-     
-   
   );
 }
